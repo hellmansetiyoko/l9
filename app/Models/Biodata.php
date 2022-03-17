381ffa144ea8beb149fa0a427218de2d256e3a21
unknown <helmandmasiv@gmail.com>
2022-03-17 07:51:37 +0700

    serialize attribute name on biodata model

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Biodata extends Model
@@ -11,6 +12,19 @@ class Biodata extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    protected $fillable = ['user_id', 'phone', 'address'];
+
+    protected $appends = [
+        'name'
+    ];
+
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            get: fn () => ucfirst($this->user->name),
+        );
+    }
 
     public function user()
     {
