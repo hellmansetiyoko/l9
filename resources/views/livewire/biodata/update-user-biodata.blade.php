9a747922807464706f7cddfab7f7bda3cba4b3e8
unknown <helmandmasiv@gmail.com>
2022-03-17 07:52:20 +0700

    change to text area address form view

--- resources/views/livewire/biodata/update-user-biodata.blade.php
+++ resources/views/livewire/biodata/update-user-biodata.blade.php
@@ -9,17 +9,23 @@
 
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="address" value="{{ __('Address') }}" />
-            <x-jet-input id="address" type="text" class="block w-full mt-1" wire:model.defer="state.address" autocomplete="address" />
-            <x-jet-input-error for="address" class="mt-2" />
-        </div>
 
-        <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="phone" value="{{ __('phone') }}" />
-            <x-jet-input id="phone" type="text" class="block w-full mt-1" wire:model.defer="state.phone" autocomplete="phone" />
-            <x-jet-input-error for="phone" class="mt-2" />
-        </div>
+            <div class="col-span-6 sm:col-span-4">
+                <x-jet-label for="phone" value="{{ __('phone') }}" />
+                <x-jet-input id="phone" type="text" class="block w-full mt-1" wire:model.defer="state.phone" autocomplete="phone" />
+                <x-jet-input-error for="phone" class="mt-2" />
+            </div>
 
+            <div>
+                <x-jet-label for="address" value="{{ __('Address') }}" />
+
+                <div class="mt-1">
+                  <textarea wire:model.defer="state.address"
+                  rows="4" name="comment" id="comment" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                </div>
+                <x-jet-input-error for="address" class="mt-2" />
+            </div>
+        </div>
     </x-slot>
 
     <x-slot name="actions">
